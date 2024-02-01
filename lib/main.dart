dae85bc5037ed47e4d4211cbaea673879b2134eb
tiobi <tiobi@naver.com>
2024-02-01 21:46:39 +0900

    added dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 /// Flutter and Dart packages
+///
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 /// 3rd party packages
+///
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:pedalpulse/firebase_options.dart';
+import 'package:pedalpulse/injection_container.dart';
 
 /// App packages
+///
 import 'app/pedal_pulse_app.dart';
 
 void main() async {
+  /// Initialize Dependency Injection
+  ///
+  await initializeDependencies();
+
   /// Initialize App and Firebase
+  ///
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
