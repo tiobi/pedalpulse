52ddc6dedbc32d9b910c9e44b5c808a02e797acc
tiobi <tiobi@naver.com>
2024-02-02 15:14:31 +0900

    working on auth features

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,4 +2,7 @@ import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 
-Future<void> initializeDependencies() async {}
+Future<void> initializeDependencies() async {
+  /// Firebase Storage, Cloud Firestore
+  ///
+}
