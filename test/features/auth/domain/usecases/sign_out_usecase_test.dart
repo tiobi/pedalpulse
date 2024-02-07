f434e5385adf16bd422a62cc998abfb9f216048c
tiobi <tiobi@naver.com>
2024-02-07 16:23:45 +0900

    Working on Usecase Tests

--- test/features/auth/domain/usecases/sign_out_usecase_test.dart
+++ test/features/auth/domain/usecases/sign_out_usecase_test.dart
@@ -1,26 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:pedalpulse/features/auth/domain/usecases/sign_out_usecase.dart';
-
 import '../../helpers/auth_test_helpers.dart';
 
 void main() {
-  late final SignOutUseCase signOutUseCase;
-  late final MockFirebaseAuthRepository mockFirebaseAuthRepository;
+  late MockFirebaseAuthRepository mockFirebaseAuthRepository;
+  late SignOutUseCase usecase;
 
   setUp(() {
     mockFirebaseAuthRepository = MockFirebaseAuthRepository();
-    signOutUseCase = SignOutUseCase(repository: mockFirebaseAuthRepository);
+    usecase = SignOutUseCase(repository: mockFirebaseAuthRepository);
   });
 
-  test('should sign out the user and return unit', () async {
+  test('should sign out from the repository', () async {
+    // arrange
     when(mockFirebaseAuthRepository.signOut())
-        .thenAnswer((_) async => Future.value(const Right(unit)));
-
-    final result = await signOutUseCase.call();
-
-    verify(mockFirebaseAuthRepository.signOut()).called(1);
-    expect(result, const Right(unit));
+        .thenAnswer((_) async => right(unit));
+    // act
+    final result = await usecase();
+    // assert
+    expect(result, equals(Right(unit)));
+    verify(() => mockFirebaseAuthRepository.signOut()).called(1);
+    verifyNoMoreInteractions(mockFirebaseAuthRepository);
   });
 }
