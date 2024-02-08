e3e539b64162d440f2a8578bee484dcd89bd5600
tiobi <tiobi@naver.com>
2024-02-08 18:44:55 +0900

    Done Testing and Implementing Auth Usecases

--- test/features/auth/domain/usecases/sign_up_with_email_and_password_usecase_test.dart
+++ test/features/auth/domain/usecases/sign_up_with_email_and_password_usecase_test.dart
@@ -1,4 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pedalpulse/core/errors/failure.dart';
+import 'package:pedalpulse/core/errors/firebase_auth_failure.dart';
+import 'package:pedalpulse/features/auth/domain/entities/auth_entity.dart';
 import 'package:pedalpulse/features/auth/domain/usecases/sign_up_with_email_and_password_usecase.dart';
 
 import '../../mocks/mock_firebase_auth_repository.mocks.dart';
@@ -10,5 +15,62 @@ void main() {
   setUp(() {
     repository = MockFirebaseAuthRepository();
     useCase = SignUpWithEmailAndPasswordUseCase(repository: repository);
+  });
+
+  const String tEmail = 'hello@world.com';
+  const String tPassword = 'password1234';
+  const String tEmptyEmail = '';
+  const String tEmptyPassword = '';
+
+  const AuthEntity tAuthEntity = AuthEntity(
+    email: tEmail,
+    password: tPassword,
+  );
+  const AuthEntity tEmptyAuthEntity = AuthEntity(
+    email: tEmptyEmail,
+    password: tEmptyPassword,
+  );
+
+  final Failure failure = FirebaseAuthFailure(
+    'Email and password cannot be empty',
+  );
+
+  group('SignUpWithEmailAndPasswordUseCase Test', () {
+    test('should sign up the user and get user credential', () async {
+      // Arrange
+      when(repository.signUpWithEmailAndPassword(authEntity: tAuthEntity))
+          .thenAnswer((_) async => const Right(unit));
+
+      // Act
+      final result = await useCase(authEntity: tAuthEntity);
+
+      // Assert
+      expect(result, const Right(unit));
+    });
+
+    test('should return failure when empty email and password is provided',
+        () async {
+      // Arrange
+      when(repository.signUpWithEmailAndPassword(authEntity: tEmptyAuthEntity))
+          .thenAnswer((_) async => Left(failure));
+
+      // Act
+      final result = await useCase(authEntity: tEmptyAuthEntity);
+
+      // Assert
+      expect(result, Left(failure));
+    });
+
+    test('should return failure when sign up fails', () async {
+      // Arrange
+      when(repository.signUpWithEmailAndPassword(authEntity: tAuthEntity))
+          .thenAnswer((_) async => Left(failure));
+
+      // Act
+      final result = await useCase(authEntity: tAuthEntity);
+
+      // Assert
+      expect(result, Left(failure));
+    });
   });
 }
