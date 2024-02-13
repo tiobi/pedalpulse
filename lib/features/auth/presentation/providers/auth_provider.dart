3e5df110b0bbd836ac04aecd5f83174b64f06fbb
tiobi <tiobi@naver.com>
2024-02-13 17:19:39 +0900

    Working on Auth Provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pedalpulse/features/auth/domain/usecases/is_email_verified_usecase.dart';
-import 'package:pedalpulse/features/auth/domain/usecases/send_password_reset_email_usecase.dart';
-import 'package:pedalpulse/features/auth/domain/usecases/sign_in_with_email_and_password_usecase.dart';
-import 'package:pedalpulse/features/auth/domain/usecases/sign_out_usecase.dart';
-import 'package:pedalpulse/features/auth/domain/usecases/sign_up_with_email_and_password_usecase.dart';
+import '../../domain/usecases/is_email_verified_usecase.dart';
+import '../../domain/usecases/send_password_reset_email_usecase.dart';
+import '../../domain/usecases/sign_in_with_email_and_password_usecase.dart';
+import '../../domain/usecases/sign_out_usecase.dart';
+import '../../domain/usecases/sign_up_with_email_and_password_usecase.dart';
+import '../../domain/entities/auth_entity.dart';
 
 class AuthProvider extends ChangeNotifier {
   IsEmailVerifiedUseCase isEmailVerifiedUseCase;
@@ -20,23 +21,37 @@ class AuthProvider extends ChangeNotifier {
     required this.signUpWithEmailAndPasswordUseCase,
   });
 
-  Future<void> isEmailVerified(String email) async {
-    await isEmailVerifiedUseCase(email);
+  Future<void> isEmailVerified({
+    required String email,
+  }) async {
+    final result = await isEmailVerifiedUseCase(email: email);
+
+    if (result.isRight()) {}
   }
 
-  Future<void> sendPasswordResetEmail(String email) async {
-    await sendPasswordResetEmailUseCase(email);
+  Future<void> sendPasswordResetEmail({
+    required String email,
+  }) async {
+    await sendPasswordResetEmailUseCase(email: email);
   }
 
-  Future<void> signInWithEmailAndPassword(String email, String password) async {
-    await signInWithEmailAndPasswordUseCase(email, password);
+  Future<void> signInWithEmailAndPassword({
+    required AuthEntity authEntity,
+  }) async {
+    await signInWithEmailAndPasswordUseCase(authEntity: authEntity);
   }
 
   Future<void> signOut() async {
-    await signOutUseCase();
+    final result = await signOutUseCase();
+
+    if (result.isRight()) {
+      notifyListeners();
+    } else {}
   }
 
-  Future<void> signUpWithEmailAndPassword(String email, String password) async {
-    await signUpWithEmailAndPasswordUseCase(email, password);
+  Future<void> signUpWithEmailAndPassword({
+    required AuthEntity authEntity,
+  }) async {
+    await signUpWithEmailAndPasswordUseCase(authEntity: authEntity);
   }
 }
